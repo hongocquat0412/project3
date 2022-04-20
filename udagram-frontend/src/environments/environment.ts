b25964b50584b8ddbe07ffa24c10053fb43f6dca
quathn1 <hongocquat0412@gmail.com>
2022-04-20 22:06:46 +0700

    update

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ae9c8ef3ed9f6451fbe94adbf29ae723-2028294643.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
